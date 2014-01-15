0906d93cda52c26de40e769feccf2ab45d6e3870
Mike Stone <mikestone14@gmail.com>
2014-01-15 13:45:12 -0500

    Program takes a stock symbol and outputs the price

--- stock_quotes.rb
+++ stock_quotes.rb
@@ -2,3 +2,8 @@ require 'yahoo_finance'
 require 'pry'
 
 # Your Code Here
+puts "What stock ticker symbol?"
+ticker = gets.chomp.upcase
+
+data = YahooFinance.quotes([ticker], [:last_trade_price])
+puts data[0].last_trade_price
