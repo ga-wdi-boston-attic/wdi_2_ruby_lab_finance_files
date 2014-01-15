0decbf16436480bf83f2056e1c503d726599534f
Mike Stone <mikestone14@gmail.com>
2014-01-15 14:30:46 -0500

    Computer announces stock symbol and trade price

--- stock_quotes.rb
+++ stock_quotes.rb
@@ -6,8 +6,11 @@ File.open('quotes.csv', 'a') do |f|
 
 	data = YahooFinance.quotes(ticker, [:last_trade_price])
 
-	data.each do |symbol|
-		puts "#{symbol.symbol.upcase} is trading at #{symbol.last_trade_price}"
-		f.puts "#{symbol.symbol.upcase}, #{symbol.last_trade_price}, #{time.month}/#{time.day}/#{time.year} at #{time.hour > 12 ? (time.hour - 12) : time.hour}:#{time.min}#{time.hour > 12 ? 'pm' : 'am'}"
+	data.each do |symbol_data|
+		puts "#{symbol_data.symbol.upcase} is trading at #{symbol_data.last_trade_price}"
+
+		%x{say "#{symbol_data.symbol} is trading at #{symbol_data.last_trade_price}"}
+
+		f.puts "#{symbol_data.symbol.upcase}, #{symbol_data.last_trade_price}, #{time.month}/#{time.day}/#{time.year} at #{time.hour > 12 ? (time.hour - 12) : time.hour}:#{time.min}#{time.hour > 12 ? 'pm' : 'am'}"
 	end
 end
