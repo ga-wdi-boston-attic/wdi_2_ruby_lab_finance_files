36a1a751362b0d568d02a629d0d72ab2a3311724
K Mak <karen.mak@stx.oxon.org>
2014-01-15 16:44:11 -0500

    wrote result to file, but still can only lookup one symbol at a time

--- stock_quotes.rb
+++ stock_quotes.rb
@@ -5,16 +5,10 @@ data_symbols = []
 puts "Please enter valid symbols of the stocks, indexes, exchange rates, you are looking up."
 data_symbols << gets.chomp # add answer to end of array
 
-data = YahooFinance.quotes(data_symbols, [:last_trade_price])
+data_array = YahooFinance.quotes(data_symbols, [:last_trade_price])
 
-puts data
+last_trade_price = data_array[0][:last_trade_price]
 
-# data = YahooFinance.quotes(data_symbols, data_fields, { raw: false } )
-# data[0].ask
 
-# filename = 'quotes.csv'
-# File.open(filename, 'a')
-#   file.puts
-# file.close
-
+File.open('quotes.csv', 'a') { |f| f.write("The last trading price for #{data_array[0]} is #{last_trade_price}.") }
 
