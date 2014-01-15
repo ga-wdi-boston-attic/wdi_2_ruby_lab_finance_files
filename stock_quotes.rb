3dbbfb62393f7435b6b961055ed5583428bbada2
Mike Stone <mikestone14@gmail.com>
2014-01-15 13:57:54 -0500

    Changed output file to .csv file. Refactored program to take the symbol from the command line

--- stock_quotes.rb
+++ stock_quotes.rb
@@ -1,17 +1,12 @@
 require 'yahoo_finance'
 require 'pry'
 
-# Your Code Here
-puts "What stock ticker symbol?"
-ticker = gets.chomp.upcase
+ticker = ARGV[0].upcase
 
 data = YahooFinance.quotes([ticker], [:last_trade_price])
 puts data[0].last_trade_price
 
 
-file = File.open('history.txt', 'a')
-file.puts "#{ticker}, #{data[0].last_trade_price}"
-
-
-
+file = File.open('quotes.csv', 'a')
+file.puts "#{ticker}, #{data[0].last_trade_price}, #{Time.new}"
 file.close
