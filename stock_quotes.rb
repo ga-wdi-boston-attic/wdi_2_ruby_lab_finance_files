1573c051488b5abdd42e93e7469c90b0e61a92f9
Laure Fischer <laurefischer27@gmail.com>
2014-01-15 14:35:02 -0500

    initial commit

--- stock_quotes.rb
+++ stock_quotes.rb
@@ -2,3 +2,19 @@ require 'yahoo_finance'
 require 'pry'
 
 # Your Code Here
+puts 'Please enter a stock symbol: '
+array = []
+array.push(gets.chomp)
+
+data = YahooFinance.quotes(array, [:last_trade_price])
+
+symbol = data[0].symbol
+price = data[0].ask
+
+puts "#{symbol} value is: #{price}"
+
+#File.open('quotes.csv', 'a+') { |file| file.write trade_price }
+
+file = File.open('quotes.csv', 'a+')
+file.puts "#{symbol} value is: #{price} at #{Time.now}"
+
