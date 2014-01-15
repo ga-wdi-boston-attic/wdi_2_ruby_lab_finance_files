5e8c04c8afccc1c0d1ffee04a59f53a72fad94c7
tsyan <anna.tsykalova@gmail.com>
2014-01-15 15:05:29 -0500

    adds most recent search to quotes.csv

--- stock_quotes.rb
+++ stock_quotes.rb
@@ -1,8 +1,8 @@
 require 'yahoo_finance'
 require 'pry'
 
-# prompt the user for a stock ticker symbol (e.g. "AAPL")
-# display the current stock price for this symbol
+# prompt the user for a stock ticker symbol (e.g. "AAPL") - check
+# display the current stock price for this symbol - check
 # save the result to a quotes.csv file (symbol, stock price, time)
 # display all prior search results from that quotes.csv file
 
@@ -14,12 +14,18 @@ require 'pry'
 ######################################
 
 puts "Please enter a stock ticker symbol:"
-stock_symbol = gets.chomp()
+stock_symbol = gets.chomp().upcase
 
-price_array = YahooFinance.quotes([stock_symbol.to_s], [:last_trade_price]) # => [symbol="AAPL", last_trade_price="557.38"], array of 2 elements
-price = price_array[0][:last_trade_price]
+price_array = YahooFinance.quotes([stock_symbol], [:last_trade_price]) # => [symbol="AAPL", last_trade_price="557.38"], array of 2 elements
+price = price_array[0][:last_trade_price].to_f
 
 puts "The current stock price is:"
 puts price
 
+quote = [stock_symbol, price, Time.now].inspect
 
+prices_file = File.open("quotes.csv", "a+").puts(quote)
+
+puts "The quote has been added to quotes.csv"
+
+#######################################
