0fb28d90dd8cb043fcb50d7b68807def7918d23e
Mike Stone <mikestone14@gmail.com>
2014-01-15 14:23:29 -0500

    Improved time format for output

--- stock_quotes.rb
+++ stock_quotes.rb
@@ -2,11 +2,12 @@ require 'yahoo_finance'
 require 'pry'
 File.open('quotes.csv', 'a') do |f|
 	ticker = ARGV
+	time = Time.new
 
 	data = YahooFinance.quotes(ticker, [:last_trade_price])
 
 	data.each do |symbol|
 		puts "#{symbol.symbol.upcase} is trading at #{symbol.last_trade_price}"
-		f.puts "#{symbol.symbol.upcase}, #{symbol.last_trade_price}, #{Time.new}"
+		f.puts "#{symbol.symbol.upcase}, #{symbol.last_trade_price}, #{time.month}/#{time.day}/#{time.year} at #{time.hour > 12 ? (time.hour - 12) : time.hour}:#{time.min}#{time.hour > 12 ? 'pm' : 'am'}"
 	end
 end
