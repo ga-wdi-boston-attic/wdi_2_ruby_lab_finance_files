112dda5b2151ac575d09949c996081e94eb41d26
Janice Smith <janice.k.n.smith@gmail.com>
2014-01-15 14:36:04 -0500

    can get stock price and write it to file - can't yet display

--- stock_quotes.rb
+++ stock_quotes.rb
@@ -1,4 +1,36 @@
 require 'yahoo_finance'
 require 'pry'
 
-# Your Code Here
+# Apple = AAPL
+# YHOO = Yahoo
+
+# Create a program that will prompt the user for a stock ticker symbol.
+def get_input(prompt)
+	puts prompt
+	gets.chomp
+end
+
+stock_ticker_symbol = get_input("stock ticker symbol of the stock you want to check:")
+
+
+data = YahooFinance.quotes([stock_ticker_symbol], [:last_trade_price] )
+puts data
+
+data_hash = {}
+
+data_hash = { Time.now.to_s => data }
+
+# Display all prior search results (symbol, price, Time)
+
+File.open("prev_trades_search.txt").readlines.each do |line|
+   puts line
+end
+
+
+# Append the ticker symbol, current price and current Time to the quotes.csv file
+# so that on the next run of the program, this information will be saved.
+
+f_name = "prev_trades_search.csv"
+file = File.open(f_name, "a")
+file.puts data_hash
+file.close
