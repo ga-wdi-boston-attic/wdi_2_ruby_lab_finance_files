7cd7f2ff804efefb98abaf06d374130649d1b767
K Mak <karen.mak@stx.oxon.org>
2014-01-15 21:50:03 -0500

    final version - push again

--- stock_quotes.rb
+++ stock_quotes.rb
@@ -3,12 +3,10 @@ require 'pry'
 
 data_symbols = []
 puts "Please enter valid symbols of the stocks, indexes, exchange rates, you are looking up."
-data_symbols << gets.chomp # add answer to end of array
+data_symbols << gets.chomp.to_s # add answer to end of array
 
-data_array = YahooFinance.quotes(data_symbols, [:last_trade_price])
+data = YahooFinance.quotes(data_symbols, [:last_trade_price]) #data is an array
 
-last_trade_price = data_array[0][:last_trade_price]
+last_trade_price = data[0][:last_trade_price]
 
-
-File.open('quotes.csv', 'a') { |f| f.write("The last trading price for #{data_array[0]} is #{last_trade_price}.") }
-
+File.open('quotes.csv', 'a') { |f| f.write("The last trading price for #{data_symbols} is #{last_trade_price}.") }
