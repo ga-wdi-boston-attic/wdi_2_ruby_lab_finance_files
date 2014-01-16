cd4dc09fc0cf25880a1b0aff03e117a5a069101b
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-01-15 22:29:27 -0500

    couldn't make it work the way i wanted, admitted defeat, gave in to only one input value

--- stock_quotes.rb
+++ stock_quotes.rb
@@ -1,16 +1,13 @@
 require 'yahoo_finance'
 require 'pry'
 
-# Your Code Here
-puts "Enter the ticker symbol(s) for your desired stocks. If entering more than one, separate them with a comma (',')."
-gets.chomp
+puts "Enter the ticker symbol for your desired stock."
+stock_info = []
+stock_info.push(gets.chomp)
+data = YahooFinance.quotes(stock_info, [:last_trade_price])
+price = data[0].last_trade_price
+ticker = data[0].symbol
 
-input = gets.chomp
-a = []
-stock_ticker = input.split(',')
-data = YahooFinance.quotes([stock_ticker], [:bid])
-stock_ticker = data[0]
+puts  "As of now, " + "#{ticker}" +"'s" + " value is " + "#{price}" + "."
 
-puts data[0].symbol + " value is: " + data[0].bid
-
-binding.pry
+File.open('quotes.csv', 'a+') { |file| file.write "#{ticker} value is: #{price} at #{Time.now}" }
